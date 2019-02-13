e8d59a0447c9394207d17c9a0bb6f8c83e42eaef
Tiago Almeida <tiagu.almeida@gmail.com>
2019-02-13 01:51:19 -0200

    att text colors

--- src/screens/home/styles.ts
+++ src/screens/home/styles.ts
@@ -1,6 +1,6 @@
-import styled from "styled-components/native";
-import { colors, metrics } from "src/styles";
-import { SafeAreaView } from "react-navigation";
+import styled from 'styled-components/native'
+import { colors, metrics } from 'src/styles'
+import { SafeAreaView } from 'react-navigation'
 
 export const BackgroundImage = styled.Image`
   width: 100%;
@@ -9,48 +9,49 @@ export const BackgroundImage = styled.Image`
   top: 0;
   left: 0;
   opacity: 0.3;
-`;
+`
 
 export const Container = styled.View`
   flex: 1;
   background-color: ${colors.primary};
   align-items: center;
-`;
+`
 
 export const Content = styled.View`
   padding: 10px;
   justify-content: center;
   align-items: center;
   flex: 1;
-`;
+`
 
 export const Row = styled.View`
   flex-direction: row;
-`;
+`
 
 export const Title = styled.Text.attrs(({ title }) => ({
-  title: title < 40 ? 25 : 18
+  title: title < 40 ? 25 : 18,
 }))`
   font-weight: bold;
   font-family: Roboto-Bold;
   font-size: ${props => props.title}px;
   margin-bottom: 5px;
-  color: #2C2605;
-`;
+  color: ${colors.text};
+`
 
 export const SubTitle = styled.Text`
   font-weight: 600;
   font-family: Roboto-Medium;
   font-size: 18px;
   margin-bottom: ${metrics.baseMargin}px;
-`;
+  color: ${colors.text};
+`
 
 export const Input = styled.TextInput.attrs({
   autoFocus: false,
-  placeholder: "search books",
-  autoCapitalize: "none",
+  placeholder: 'search books',
+  autoCapitalize: 'none',
   autoCorrect: false,
-  underlineColorAndroid: "transparent"
+  underlineColorAndroid: 'transparent',
 })`
   padding: 50px 5px;
   max-width: 250px;
@@ -61,7 +62,7 @@ export const Input = styled.TextInput.attrs({
   padding: 5px 10px;
   margin-bottom: ${metrics.baseMargin}px;
   flex: 1;
-`;
+`
 
 export const Button = styled.TouchableOpacity`
   max-width: 250px;
@@ -72,11 +73,11 @@ export const Button = styled.TouchableOpacity`
   padding: 10px 30px;
   margin-bottom: ${metrics.baseMargin}px;
   flex: 1;
-`;
+`
 
 export const TextButton = styled.Text`
   color: white;
   font-weight: 500;
   font-family: Roboto-Regular;
   font-size: 16px;
-`;
+`
--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,23 +1,24 @@
-import Color from "color";
+import Color from 'color'
 
 export default {
-  white: "#FFF",
-  lighter: "#EEE",
-  light: "#DDD",
-  regular: "#999",
-  dark: "#666",
-  darker: "#333",
-  black: "#000",
+  white: '#FFF',
+  lighter: '#EEE',
+  light: '#DDD',
+  regular: '#999',
+  dark: '#666',
+  darker: '#333',
+  black: '#000',
 
-  primary: "#FFDD0D",
-  primaryDark: Color("#FFDD0D").darken(0.1),
-  secondary: "#6ADC00",
-  success: "#9DCA83",
-  danger: "#E37A7A",
+  primary: '#FFDD0D',
+  primaryDark: Color('#FFDD0D').darken(0.1),
+  secondary: '#6ADC00',
+  success: '#9DCA83',
+  danger: '#E37A7A',
 
-  transparent: "transparent",
-  darkTransparent: "rgba(0, 0, 0, 0.6)",
-  whiteTransparent: "rgba(255, 255, 255, 0.3)",
+  transparent: 'transparent',
+  darkTransparent: 'rgba(0, 0, 0, 0.6)',
+  whiteTransparent: 'rgba(255, 255, 255, 0.3)',
 
-  button: "blue"
-};
+  button: '#4A90E2',
+  text: '#2C2605',
+}
